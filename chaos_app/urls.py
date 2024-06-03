299af57d65f9e0fc188ff38783e2778cf1e7898a
EminDrn <1eminduran@gmail.com>
2024-06-03 11:56:23 +0300

    tinkerbell api added

--- chaos_app/urls.py
+++ chaos_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 urlpatterns = [
 
     path('api/generate_and_save_tent_map/', views.generate_and_save_tent_map, name="generate_and_save_tent_map"),
-    path('api/generate_and_save_logistic_map/', views.generate_and_save_logistic_map, name='generate_and_save_logistic_map'),
+    path('api/generate_and_save_logistic_map/', views.generate_and_save_logistic_map,
+         name="generate_and_save_logistic_map"),
     path('api/generate_and_save_complex_squaring_map/', views.generate_and_save_complex_squaring_map, name='generate_and_save_complex_squaring_map'),
     
     path('api/arnoldcat_map/', views.arnoldcat_map_api, name='arnoldcat_map_api'),
@@ -12,7 +13,7 @@ urlpatterns = [
 
     path('api/lorenz_map/', views.lorenz_map, name='lorenz_map'),
     path('api/poincare_map/', views.poincare_map_view, name='poincare_map'),
-    path('api/generate_tinkerbell_map/', views.generate_and_save_tinkerbell_map, name='generate_tinkerbell_map'),
+    path('api/generate_and_save_tinkerbell_map/', views.generate_and_save_tinkerbell_map, name='generate_and_save_tinkerbell_map'),
     path('api/generate_logistic_map/', views.generate_and_save_logistic_map, name='generate_logistic_map'),
 
     path('api/generate_and_save_gauss_map/', views.generate_and_save_gauss_map,name='generate_and_save_gauss_map'),
